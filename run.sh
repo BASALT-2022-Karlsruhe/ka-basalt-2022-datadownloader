7b3d1f55a94d1f6b37bbd758b0237d1944372f4b
Walter Laurito <Lauritowal@yahoo.com>
2022-08-20 13:49:09 +0200

    add -d to docker run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # build and run container - the data will be downloaded to the created data folder
-docker-compose up --build # -d
+docker-compose up --build -d
 
 docker volume inspect ka-basalt-2022-datadownloader_kabasalt_data
