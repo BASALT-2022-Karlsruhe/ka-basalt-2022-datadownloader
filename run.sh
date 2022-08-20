e54c463b50e80df444bef43397564f1314d1c271
Walter Laurito <Lauritowal@yahoo.com>
2022-08-20 18:06:26 +0200

    add information about deleting volume to run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# Might need to uncomment when doing changes to the Dockerfile
+# docker volume rm ka-basalt-2022-datadownloader_data
+
 # build and run container - the data will be downloaded to the created data folder
 docker-compose up --build -d
 
-docker volume inspect ka-basalt-2022-datadownloader_kabasalt_data
+docker volume inspect ka-basalt-2022-datadownloader_data
