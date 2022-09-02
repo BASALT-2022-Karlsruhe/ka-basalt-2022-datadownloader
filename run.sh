d7940f4e86f295b409a81c31d2c45199729281a4
Walter Laurito <Lauritowal@yahoo.com>
2022-09-02 21:38:03 +0200

    small comments correction

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # docker volume rm ka-basalt-2022-datadownloader_data
 
 # build and run container - the data will be downloaded to the created data folder
-docker-compose up -d
+docker-compose up -d --build
 
 docker volume inspect ka-basalt-2022-datadownloader_data
 
