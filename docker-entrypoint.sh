e67e48395dd48336a7fd1d92ce8584d4f283d456
rk1a <96744960+rk1a@users.noreply.github.com>
2022-10-06 00:40:42 +0200

    Add larger and finetuned models to the repo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,20 @@ python3 download_dataset.py --json-file /home/resources/find-cave-Jul-28.json --
 python3 download_dataset.py --json-file /home/resources/build-house-Jul-28.json --output-dir /home/data/MineRLBasaltBuildVillageHouse-v0 --num-demos ${NUM_VILLAGE_DEMOS}
 python3 download_dataset.py --json-file /home/resources/pen-animals-Jul-28.json --output-dir /home/data/MineRLBasaltCreateVillageAnimalPen-v0 --num-demos ${NUM_ANIMAL_DEMOS}
 python3 download_dataset.py --json-file /home/resources/waterfall-Jul-28.json --output-dir /home/data/MineRLBasaltMakeWaterfall-v0 --num-demos ${NUM_WATERFALL_DEMOS}
-## Model & Weights
+## Base Models & Weights
+## width 1x
 wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-1x.model -P /home/data/VPT-models
 wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-1x.weights -P /home/data/VPT-models
+## width 2x
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/2x.model -P /home/data/VPT-models
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-2x.weights -P /home/data/VPT-models
+## width 3x
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-3x.model -P /home/data/VPT-models
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-3x.weights -P /home/data/VPT-models
+## BC finetuned on house building, width 3x
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/bc-house-3x.weights -P /home/data/VPT-models
+## RL finetuned for crafting diamond pickaxe ASAP, width 2x
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/rl-from-foundation-2x.weights -P /home/data/VPT-models
 
 chown -R 1001:1001 /home/data/
 chown -R 1001:1001 /home/data/VPT-models
